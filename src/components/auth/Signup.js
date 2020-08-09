02c99d98e9e5ece22f0886aab0d53a2458c5a7d6
jbryants <jbryants19d@gmail.com>
2020-08-09 16:30:14 +0530

    feature component and requireAuth higher order component to manage access to components

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 class Signup extends Component {
   onSubmit = (formProps) => {
-    console.log(formProps);
+    this.props.signup(formProps, () => {
+      this.props.history.push("/feature");
+    });
   };
 
   render() {
@@ -29,10 +34,19 @@ class Signup extends Component {
             autoComplete="none"
           />
         </fieldset>
+        <div>{this.props.errorMessage}</div>
         <button>Sign up</button>
       </form>
     );
   }
 }
 
-export default reduxForm({ form: "signup" })(Signup);
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.errorMessage };
+}
+
+// Compose helps us to apply multiple higher order components to a single component
+export default compose(
+  connect(mapStateToProps, actions),
+  reduxForm({ form: "signup" })
+)(Signup);
